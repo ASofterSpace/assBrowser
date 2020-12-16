c241847160ce6ad0bf887039958c35cba1464d5e
Tom Moya Schau <moya@asofterspace.com>
2020-12-16 20:26:09 +0100

    Initial skeleton commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# assBrowser
+
+**Class:** Utility
+
+**Language:** Java
+
+**Platform:** Windows / Linux
+
+A general entrypoint to files and folders on a computer, modeled after the good old datacom x
+
+## Setup
+
+Download our Toolbox-Java (which is a separate project here on github) into an adjacent directory on your hard drive.
+
+Start the build by calling under Windows:
+
+```
+build.bat
+```
+
+Or under Linux:
+
+```
+build.sh
+```
+
+## Run
+
+To start up the assBrowser project after it has been built, you can call under Windows:
+
+```
+run.bat
+```
+
+Or under Linux:
+
+```
+run.sh
+```
+
+## License
+
+We at A Softer Space really love the Unlicense, which pretty much allows anyone to do anything with this source code.
+For more info, see the file UNLICENSE.
+
+If you desperately need to use this source code under a different license, [contact us](mailto:moya@asofterspace.com) - I am sure we can figure something out.
---
+++ release.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "Re-building with target Java 7 (such that the compiled .class files will be compatible with as many JVMs as possible)..."
+
+cd src
+
+# build build build!
+javac -encoding utf8 -d ../bin -bootclasspath ../other/java7_rt.jar -source 1.7 -target 1.7 @sourcefiles.list
+
+cd ..
+
+
+
+echo "Creating the release file assBrowser.zip..."
+
+mkdir release
+
+cd release
+
+mkdir assBrowser
+
+# copy the main files
+cp -R ../bin assBrowser
+cp ../UNLICENSE assBrowser
+cp ../README.md assBrowser
+cp ../run.sh assBrowser
+cp ../run.bat assBrowser
+
+# convert \n to \r\n for the Windows files!
+cd assBrowser
+awk 1 ORS='\r\n' run.bat > rn
+mv rn run.bat
+cd ..
+
+# create a version tag right in the zip file
+cd assBrowser
+version=$(./run.sh --version_for_zip)
+echo "$version" > "$version"
+cd ..
+
+# zip it all up
+zip -rq assBrowser.zip assBrowser
+
+mv assBrowser.zip ..
+
+cd ..
+rm -rf release
+
+echo "The file assBrowser.zip has been created in $(pwd)"
