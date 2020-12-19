82176f96b8564b176202dc70d2e1bd09320e47cd
Tom Moya Schau <moya@asofterspace.com>
2020-12-19 10:46:56 +0100

    Improve console behavior

--- src/com/asofterspace/assBrowser/console/ConsoleCtrl.java
+++ src/com/asofterspace/assBrowser/console/ConsoleCtrl.java
@@ -6,6 +6,7 @@ package com.asofterspace.assBrowser.console;
 
 import com.asofterspace.assBrowser.Database;
 import com.asofterspace.assBrowser.paths.PathCtrl;
+import com.asofterspace.toolbox.utils.StrUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,21 +61,43 @@ public class ConsoleCtrl {
 			return PathCtrl.DESKTOP;
 		}
 		if (command.startsWith("cd ")) {
-			command = command.substring(3).trim();
-			if (command.equals(".")) {
-				return previousPath;
+			command = command.substring(3).trim() + "/";
+			command = StrUtils.replaceAll(command, "\\", "/");
+
+			// if we have ~/blubb...
+			if (command.startsWith("~")) {
+				// then we have /Desktop/blubb
+				previousPath = PathCtrl.DESKTOP;
+				command = command.substring(2);
 			}
-			if (command.equals("..")) {
-				String result = PathCtrl.oneUp(previousPath);
-				if (result.length() < 2) {
-					if (PathCtrl.startsWithDesktopPath(previousPath)) {
-						result = PathCtrl.oneUp(database.getDesktopLocation());
+			command = StrUtils.replaceAll(command, "\\", "/");
+			command = StrUtils.replaceAll(command, "//", "/");
+
+			// if we have C:/, D:/, stuff like that...
+			if (command.length() > 2) {
+				if ((command.charAt(1) == ':') && (command.charAt(2) == '/')) {
+					// use an absolute path!
+					previousPath = "";
+				}
+			}
+
+			while (command.startsWith("./")) {
+				command = command.substring(2);
+			}
+
+			while (command.equals("..") || command.startsWith("../")) {
+				if (command.equals("..")) {
+					command = "";
+				} else {
+					command = command.substring(3);
+				}
+				String previousPreviousPath = previousPath;
+				previousPath = PathCtrl.oneUp(previousPath);
+				if (previousPath.length() < 2) {
+					if (PathCtrl.startsWithDesktopPath(previousPreviousPath)) {
+						previousPath = PathCtrl.oneUp(database.getDesktopLocation());
 					}
 				}
-				return result;
-			}
-			if (command.equals("~")) {
-				return PathCtrl.DESKTOP;
 			}
 			return previousPath + "/" + command;
 		}
